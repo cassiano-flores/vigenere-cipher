8eb9e8452f8e63f2161d2239a5aa8f4331e00dc5
cassiano-flores <cassiano.flores2424@gmail.com>
2024-03-27 18:36:30 -0300

    decode english plaintext done

--- main.py
+++ main.py
@@ -1,92 +1,106 @@
-def contar_frequencias(texto):
-  frequencias = {}
+# Autores:
+# Cassiano Luis Flores Michel  (20204012-7)
+# José Eduardo Rodrigues Serpa (20200311-7)
 
-  for letra in texto:
-    if letra in frequencias:
-      frequencias[letra] += 1
-    else:
-      frequencias[letra] = 1
+import collections
+import math
 
-  return frequencias
+global TAMANHO_CHAVE_MAXIMO
+global TEXTO_CIFRADO_INGLES
+global COINCIDENCIA_INGLES
+global LETRA_FREQUENTE_INGLES
+global TEXTO_CIFRADO_PORTUGUES
+global COINCIDENCIA_PORTUGUES
+global LETRA_FREQUENTE_PORTUGUES
 
+TAMANHO_CHAVE_MAXIMO = 20
+COINCIDENCIA_INGLES = 0.065
+COINCIDENCIA_PORTUGUES = 0.072
+LETRA_FREQUENTE_INGLES = 'E'
+LETRA_FREQUENTE_PORTUGUES = 'A'
 
-def calcular_IC(texto):
-  total_letras = len(texto)
-  frequencias = contar_frequencias(texto)
-  letras_ic = {}
+#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#
+# cálculo do índice de coincidência
+def indice_coincidencia(texto):
+    total_caracteres = len(texto)
+    frequencia = collections.Counter(texto)
+    ic = 0
 
-  for frequencia in frequencias:
-    letras_ic[frequencia] = (frequencias[frequencia] * (frequencias[frequencia] - 1)) / (total_letras * (total_letras - 1))
+    for letra in frequencia:
+        ic += (frequencia[letra] * (frequencia[letra] - 1))
 
-  return letras_ic
+    ic /= (total_caracteres * (total_caracteres - 1))
+    return ic
 
+# dividir o texto em partes
+def dividir_texto(texto, tamanho):
+    partes = [''] * tamanho
 
-def dividir_texto_em_grupos(texto, tamanho):
-  grupos = []
+    for i, caracter in enumerate(texto):
+        partes[i % tamanho] += caracter
 
-  for i in range(0, len(texto), tamanho):
-    grupos.append(texto[i:i+tamanho])
+    return partes
 
-  if len(grupos[-1]) != tamanho:
-    grupos.pop()
+# cálculo do índice de coincidência médio
+def calcular_ic_medio(texto, tamanho_chave):
+    partes = dividir_texto(texto, tamanho_chave)
+    ic_medio = sum(indice_coincidencia(parte) for parte in partes) / len(partes)
 
-  return grupos
+    return ic_medio
 
+# encontrar o tamanho da chave
+def encontrar_tamanho_chave(texto_cifrado, idioma):
+    ic_minimo = float('inf')
+    tamanho_chave = 0
 
-def descobrir_tamanho_chave(texto_cifrado):
-  english_ic = 0.0686
-  melhor_ic = 0
-  melhor_tamanho_chave = 0
+    for tamanho in range(2, TAMANHO_CHAVE_MAXIMO + 1):
+        ic_medio = calcular_ic_medio(texto_cifrado, tamanho)
 
-  for tamanho_chave_teste in range(2, 21):
-    grupos = dividir_texto_em_grupos(texto_cifrado, tamanho_chave_teste)
+        if idioma == 'us':
+          if abs(ic_medio - COINCIDENCIA_INGLES) < ic_minimo:
+              ic_minimo = abs(ic_medio - COINCIDENCIA_INGLES)
+              tamanho_chave = tamanho
+        elif idioma == 'pt':
+            if abs(ic_medio - COINCIDENCIA_PORTUGUES) < ic_minimo:
+              ic_minimo = abs(ic_medio - COINCIDENCIA_PORTUGUES)
+              tamanho_chave = tamanho
 
-    for grupo in grupos:
-      ic_grupo = calcular_IC(grupo)
-      media_ic_grupo = sum(ic_grupo.values()) / len(ic_grupo)
+    return tamanho_chave
 
-      if abs(media_ic_grupo - english_ic) < abs(melhor_ic - english_ic):
-        melhor_ic = media_ic_grupo
-        melhor_tamanho_chave = tamanho_chave_teste
-  
-  return melhor_tamanho_chave
+# decifrar o texto cifrado com cifra de Vigenère
+def decifrar_vigenere(texto_cifrado, tamanho_chave, idioma):
+    partes = dividir_texto(texto_cifrado, tamanho_chave)
+    chave = ''
 
+    for parte in partes:
+        frequencia = collections.Counter(parte)
+        letra_mais_frequente = max(frequencia, key=frequencia.get)
 
-# TESTANDO
-with open("teste.txt", "r") as file:
-  texto = file.read().replace('\n', '')
+        if idioma == 'us':
+          chave += chr((ord(letra_mais_frequente) - ord(LETRA_FREQUENTE_INGLES)) % 26 + ord('A'))
+        elif idioma == 'pt':
+          chave += chr((ord(letra_mais_frequente) - ord(LETRA_FREQUENTE_PORTUGUES)) % 26 + ord('A'))
 
-# with open("plaintext-portuguese.txt", "r") as file:
-  # texto = file.read().replace('\n', '')
+    texto_decifrado = ''
+    for i, caracter in enumerate(texto_cifrado):
+        chave_atual = chave[i % tamanho_chave]
+        texto_decifrado += chr((ord(caracter) - ord(chave_atual)) % 26 + ord('A'))
 
-def descobrir_chave(tamanho_da_chave):
-    texto_dividido = dividir_texto_em_grupos(texto,tamanho_da_chave)
-    letras_melhor_ic = {}
-    
-    for grupo in texto_dividido:
-        for i in range(tamanho_da_chave):
-            if i in letras_melhor_ic:
-                letras_melhor_ic[i] += grupo[i]
-            else:
-                letras_melhor_ic[i] = grupo[i]
-    
-    for i in letras_melhor_ic:
-        letras_melhor_ic[i] = contar_frequencias(letras_melhor_ic[i])
+    return texto_decifrado
 
-    for i in letras_melhor_ic:
-       letras_melhor_ic[i] = ord(get_dict_max(letras_melhor_ic[i])) - ord('e')
-    
-    return letras_melhor_ic
+#----------#----------#----------#----------#----------#----------#----------#----------#----------#----------#
+# execução do programa
+with open("plaintext-english.txt", "r") as file:
+  TEXTO_CIFRADO_INGLES = file.read().replace('\n', '')
 
-def get_dict_max(dict):
-  max = -1;
-  letter = "";
-  for e in dict.keys():
-    if(dict[e] >= max):
-      max = dict[e]
-      letter = e
+with open("plaintext-portuguese.txt", "r") as file:
+  TEXTO_CIFRADO_PORTUGUES = file.read().replace('\n', '')
 
-  return letter
+texto_decifrado_ingles = decifrar_vigenere(TEXTO_CIFRADO_INGLES, encontrar_tamanho_chave(TEXTO_CIFRADO_INGLES, 'us'), 'us')
+texto_decifrado_portugues = decifrar_vigenere(TEXTO_CIFRADO_PORTUGUES, encontrar_tamanho_chave(TEXTO_CIFRADO_PORTUGUES, 'pt'), 'pt')
 
-print(descobrir_tamanho_chave(texto))
-print(descobrir_chave(descobrir_tamanho_chave(texto)))
+with open("texto-claro-ingles.txt", "w") as file:
+  file.write(texto_decifrado_ingles)
+
+with open("texto-claro-portugues.txt", "w") as file:
+  file.write(texto_decifrado_portugues)
